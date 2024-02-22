b7c8d114c38777be76c8db03685fef7a46d8099b
Ioannis Horgos <jhor@acta.com.gr>
2024-02-23 00:13:14 +0200

    feat: new QueryParams class for the weather api request

---
+++ Models/QueryParametersModel.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherForecastAPI.Models 
+{
+    public class QueryParametersModel 
+    {
+        private string _aqi = "no";
+
+        [Required]
+        public string City { get; set; } = string.Empty;
+        public int Days { get; set; } = 1;
+        public string Aqi 
+        {
+            get 
+            {
+                return _aqi;
+            }
+            set
+            {
+                value = value.ToLower();
+                if(value == "yes" || value == "no")
+                {
+                    _aqi = value;
+                }
+            }
+        }
+    }
+}
